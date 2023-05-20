b71e7b297271fc494026d7344f6e9b883dadbdfe
Dorela Bani <banidorela@gmail.com>
2023-05-20 16:02:29 +0200

    first

---
+++ src/main/java/com/autoservis2Sh/Autoservis2ShApplication.java
@@ -0,0 +1,13 @@
+package com.autoservis2Sh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autoservis2ShApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Autoservis2ShApplication.class, args);
+	}
+
+}
